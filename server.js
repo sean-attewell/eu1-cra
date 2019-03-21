dafc24110cf29680f32721beebde34cfa018b64b
sean-attewell <sean.attewell@gmail.com>
2019-03-21 10:41:38 +0000

    updates server.js and package.json

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// the way CRA works is that when webpack runs it creates a build folder
+// which contains the index.html and the bundle.js
+
+// __dirname is the path to wherever this is deployed on this server.
+// Can't know this info ahead of time. It's the absolute path.
+// path.join will take care of putting forwardslashes etc
+
+const pathToIndexHtml = path.join(__dirname, 'build', 'index.html');
+const pathToBuildFolder = path.join(__dirname, 'build');
+
+// allow static assets to be served off of this folder (build)!!
+app.use(exporess.static(pathToBuildFolder));
+
+
+//sendFile need absolute path to asset
+app.get('/', (req, res) => {
+    res.sendFile(pathToIndexHtml)
+})
+
+app.listen(process.env.PORT || 5000);
+
